558d5b30c9b25d580c88114a493e06578d443840
shixl <xlshiz@users.noreply.github.com>
2020-09-05 08:34:17 +0800

    remove dirty

--- gitfast.plugin.zsh
+++ gitfast.plugin.zsh
@@ -1,6 +1,11 @@
 source "${0:A:h}/git-prompt.sh"
 
 function git_prompt_info() {
+  dirty=""
+  __git_ps1 "${ZSH_THEME_GIT_PROMPT_PREFIX//\%/%%}%s${dirty//\%/%%}${ZSH_THEME_GIT_PROMPT_SUFFIX//\%/%%}"
+}
+
+function git_prompt_info_with_dirty() {
   dirty="$(parse_git_dirty)"
   __git_ps1 "${ZSH_THEME_GIT_PROMPT_PREFIX//\%/%%}%s${dirty//\%/%%}${ZSH_THEME_GIT_PROMPT_SUFFIX//\%/%%}"
 }
